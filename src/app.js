4eab4f1b1169901a367e928d073ca3af6d4af7f3
MONU MANDAL <monumandel3@gmail.com>
2025-09-28 21:20:50 +0530

    use morgan in our app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import logger from './config/logger.js';
 
 const app = express();
 
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  morgan('combined', {
+    stream: { write: message => logger.info(message.trim()) },
+  })
+);
 
 app.get('/', (req, res) => {
   logger.info('Root endpoint was called, Hello from My API!');
