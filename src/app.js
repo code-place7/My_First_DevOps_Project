611b02365066dfc1ec3be629440a6436a1a1b933
MONU MANDAL <monumandel3@gmail.com>
2025-09-28 21:10:49 +0530

    logger was called in our app

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 const app = express();
 
 app.get('/', (req, res) => {
+  logger.info('Root endpoint was called, Hello from My API!');
   res.status(200).send('Hello, from My APi!');
 });
 
