db11d395bfe1dfb65c838e6a929f4ce7d354965f
MONU MANDAL <monumandel3@gmail.com>
2025-11-27 20:03:50 +0530

    implement auth controllers

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,25 +1,25 @@
-import jwt, { verify } from 'jsonwebtoken';
-import { loggers } from 'winston';
+import jwt from 'jsonwebtoken';
+import logger from '../config/logger.js';
 
-const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key';
-const JWT_EXPIRES_IN = '1d'; // Token expiration time(
+const JWT_SECRET =
+  process.env.JWT_SECRET || 'your-secret-key-please-change-in-production';
+const JWT_EXPIRES_IN = '1d';
 
-export const generateToken = payload => {
+export const jwttoken = {
   sign: payload => {
     try {
       return jwt.sign(payload, JWT_SECRET, { expiresIn: JWT_EXPIRES_IN });
-    } catch (error) {
-      loggers.error('Error generating token:', error);
-      throw error;
+    } catch (e) {
+      logger.error('Failed to authenticate token', e);
+      throw new Error('Failed to authenticate token');
     }
-  };
-
+  },
   verify: token => {
     try {
       return jwt.verify(token, JWT_SECRET);
-    } catch (error) {
-      loggers.error('Error verifying token:', error);
-      throw error;
+    } catch (e) {
+      logger.error('Failed to authenticate token', e);
+      throw new Error('Failed to authenticate token');
     }
-  };
+  },
 };
